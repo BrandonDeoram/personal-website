00566a4e06beb5bd653d70379317d8da4b435384
BrandonDeoram <brandondeoram8931@gmail.com>
2022-10-03 16:11:47 -0400

    Finished About Me

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -16,7 +16,27 @@ export default function AboutMe() {
             </div>
             <div className='rowContainer'>
                 <div className='descriptionContainer'>
-                    I am currently a 4th-year university student studying Computer Science at Ontario Tech University. I love to create mobile applications that benefit me and my friends as well as automate easy tasks using python and other languages. I code for fun in my free time, there's just so much I want to learn and create that it's hard choosing one to project focus on.
+                    <p>
+                        I am currently a <span className='redText'>4th</span>-year university student studying Computer Science at <span className='redText'> Ontario Tech University</span>. I love to create <span className='redText'> mobile applications</span> that benefit me and my friends as well as automate easy tasks using python and other languages. I code for fun in my free time, there's just so much I want to learn and create that it's hard choosing one to project focus on.
+                    </p>
+
+                    <div className='technologyTitle'>
+                        Here are some of the tehcnologies i've used:
+                    </div>
+                    <div className='grid-container'>
+                        <ul>
+                            <li>Javascipt</li>
+                            <li>React.js</li>
+                            <li>Node.js</li>
+                            <li>Flutter</li>
+                            <li>Python</li>
+                            <li>Html/Css</li>
+                        </ul>
+                    </div>
+                    <div className='outside'>
+                        Outside of school, I love to build different keyboards , as well as computers.
+                    </div>
+
                 </div>
                 <div>
                     <img className='meContainer' src={me} alt="" />
