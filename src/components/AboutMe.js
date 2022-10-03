5f62cc5d43346a2f2dc32e01b14ea6637286eaea
BrandonDeoram <brandondeoram8931@gmail.com>
2022-10-02 22:44:16 -0400

    Added image

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import "../styles/AboutMe.css";
 import img1 from "../images/akatLogo.png";
+import me from "../images/me.jpg";
 export default function AboutMe() {
     return (
         <div id="aboutme">
@@ -8,9 +9,18 @@ export default function AboutMe() {
                 <div className='akatSymbol'>
                     <img src={img1} alt="" className='akatsukiImage' />
                 </div>
-                <p className='title'>About Me</p>
+                <div className='title'>
+                    About Me
+                </div>
                 <hr className='solid' />
-
+            </div>
+            <div className='rowContainer'>
+                <div className='descriptionContainer'>
+                    I am currently a 4th-year university student studying Computer Science at Ontario Tech University. I love to create mobile applications that benefit me and my friends as well as automate easy tasks using python and other languages. I code for fun in my free time, there's just so much I want to learn and create that it's hard choosing one to project focus on.
+                </div>
+                <div>
+                    <img className='meContainer' src={me} alt="" />
+                </div>
             </div>
         </div>
     )
